4884b39807e2e6a4f7c12976fbc9563eb81bad4e
Rafael Amaral <rafaelpaulajr@gmail.com>
2021-09-07 17:03:59 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dssalesapi/DssalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dssalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
